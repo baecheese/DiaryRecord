b203eac21ec62a15833c6b9b63cafcd45ce948bc
배지영 <melissaeliza0@gmail.com>
2017-02-01 12:24:18 +0900

    make textview

---
+++ DiaryRecord/Diary.swift
@@ -0,0 +1,17 @@
+//
+//  Diary.swift
+//  DiaryRecord
+//
+//  Created by 배지영 on 2017. 2. 1..
+//  Copyright © 2017년 baecheese. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Diary: Object {
+    
+    //dynamic var data
+    
+    
+}
